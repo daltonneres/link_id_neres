24607eabaaa22d83ad87d3d87aabd997d053ea55
Dalton josé Neres <daltonneres7@gmail.com>
2026-03-21 17:35:40 -0300

    upload

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('active');
+    } else {
+      entry.target.classList.remove('active'); // anima saída também
     }
   });
+}, {
+  threshold: 0.1, // ativa mais cedo
+  rootMargin: "0px 0px -100px 0px" // ativa antes de chegar no final da tela
 });
 
 elements.forEach(el => observer.observe(el));
